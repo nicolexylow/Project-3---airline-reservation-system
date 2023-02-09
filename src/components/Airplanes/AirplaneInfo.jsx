2a396c85b67d5b11f6ad1c7e965d62534dda5341
Nicole Low <nicolexylow@gmail.com>
2023-02-09 11:13:14 +1100

    Airplane info displayed

--- src/components/Airplanes/AirplaneInfo.jsx
+++ src/components/Airplanes/AirplaneInfo.jsx
@@ -1,24 +1,45 @@
 import React from 'react'
-import { Table } from 'semantic-ui-react'
 
 const AirplaneInfo = (props) => {
-    return (
-        <div>
-            <h2>{props.name}</h2>
 
-            {props.rows < props.cols && (
-                <Table.Row className="disabled-row">
-                    {rows.map((item, index) => {
-                    return (
-                        <Table.Cell id="cta" textAlign="right" key={index}>
-                        {item}
-                        </Table.Cell>
-                    );
-                    })}
-                </Table.Row>   
-                )}
-        </div>
+    const rowLabels = Array.from({ length: props.rows }, (_, index) =>
+    String.fromCharCode(65 + index)
+    );
+    const columnLabels = Array.from({ length: props.cols }, (_, index) =>
+    (index + 1).toString()
+    );
+
+    return(
+        <table>
+            <thead>
+                <tr>
+                <th></th>
+                {columnLabels.map((_, index) => (
+                    <th key={index}></th>
+                ))}
+                </tr>
+            </thead>
+            <tbody>
+                {rowLabels.map((label, rowIndex) => (
+                <tr key={rowIndex}>
+                    <th>{label}</th>
+                    {Array.from({ length: props.cols }, (_, columnIndex) => (
+                    <td key={columnIndex}></td>
+                    ))}
+                </tr>
+                ))}
+                <tr>
+                <th></th>
+                {columnLabels.map((label, index) => (
+                    <th key={index}>{label}</th>
+                ))}
+                </tr>
+            </tbody>
+        </table>
     )
+    
 }
+
+
 
 export default AirplaneInfo
